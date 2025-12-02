adddb48889796184ac14e6551fec8831cc47e6c4
nagaraj90199 <kn733478@gmail.com>
2025-12-02 09:01:30 +0530

    Add Employee.py and test_Employee.py files

---
+++ test_Employee.py
@@ -0,0 +1,10 @@
+from employee import Employee_details
+
+def test_employee_details():
+    expected_output = (
+        "Employee Name: Alice\n"
+        "Employee ID: E1001\n"
+        "Department: IT\n"
+        "Salary: 55000\n"
+    )
+    assert Employee_details("Alice", "E1001", "IT", "55000") == expected_output
